56294c8f5d1398b45225c4709107f7e8e7128b2c
Roman Grubic <grubic.roman@gmail.com>
2020-07-03 11:38:48 +0100

    Character grid done. Each character component created

---
+++ src/components/characters/CharacterItem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const CharacterItem = ({ item }) => {
+    return (
+        <div className='card'>
+            <div className='card-inner'>
+                <div className='card-front'></div>
+            </div>
+        </div>
+    );
+};
+
+export default CharacterItem;
