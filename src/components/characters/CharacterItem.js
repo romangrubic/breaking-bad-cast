0f0a1a57ce1b51886231bd61a296fe589671778e
Roman Grubic <grubic.roman@gmail.com>
2020-07-03 11:55:16 +0100

    Finished character item component.

--- src/components/characters/CharacterItem.js
+++ src/components/characters/CharacterItem.js
@@ -4,7 +4,32 @@ const CharacterItem = ({ item }) => {
     return (
         <div className='card'>
             <div className='card-inner'>
-                <div className='card-front'></div>
+                <div className='card-front'>
+                    <img src={item.img} alt='' />
+                </div>
+                <div className='card-back'>
+                    <h1>{item.name}</h1>
+                    <ul>
+                        <li>
+                            <strong>Actor Name:</strong> {item.portrayed}
+                        </li>
+                        <li>
+                            <strong> Nickname:</strong> {item.nickname}
+                        </li>
+                        <li>
+                            <strong>Birthday:</strong> {item.birthday}
+                        </li>
+                        <li>
+                            <strong>Occupation: </strong>
+                            {item.occupation.map((occupat) => (
+                                <span>{occupat}, </span>
+                            ))}
+                        </li>
+                        <li>
+                            <strong>Status:</strong> {item.status}
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     );
